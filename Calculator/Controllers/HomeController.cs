0cfe3ed12fa9e31c647fa3606fa598469a86f863
Norbert <wrzosowskyy@gmail.com>
2024-06-23 11:54:56 +0200

    add - * /

--- Calculator/Controllers/HomeController.cs
+++ Calculator/Controllers/HomeController.cs
@@ -24,9 +24,50 @@ public class HomeController : Controller{
         return View(new ErrorViewModel{ RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
     [HttpPost]
-    public ActionResult number(int a, int b){
-        int sum = a + b;
-        ViewBag.Sum = sum;
-        return View("Index");
+    public ActionResult Index(int a, int b, string operation)
+    {
+        try
+        {
+            ViewBag.a = a;
+            ViewBag.b = b;
+            ViewBag.Operation = operation;
+
+            switch (operation)
+            {
+                case "add":
+                    ViewBag.Result = a + b;
+                    break;
+                case "subtraction":
+                    ViewBag.Result = a - b;
+                    break;
+                case "multiplication":
+                    ViewBag.Result = a * b;
+                    break;
+                case "division":
+                    if (b != 0)
+                    {
+                        ViewBag.Result = a / (double)b; // Cast to double for division
+                    }
+                    else
+                    {
+                        ViewBag.Result = "Cannot divide by zero";
+                    }
+                    break;
+                default:
+                    ViewBag.Result = "Invalid operation";
+                    break;
+            }
+
+            return View();
+        }
+        catch (Exception ex)
+        {
+            // Log the exception (use any logging framework or simply write to a log file)
+            System.Diagnostics.Debug.WriteLine(ex.Message);
+            // Optionally, set a ViewBag property to show an error message in the view
+            ViewBag.Error = "An error occurred while processing your request.";
+            return View("Error"); // Ensure you have an Error view to show a friendly error message
+        }
+        return View();
     }
 }
--- Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
+++ Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f2cb608d31bb71f4b5b7995a528346e93b76bb0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
